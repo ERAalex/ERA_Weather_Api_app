8c863911c3938c60a895c1602e53d0c0d6bdb22e
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-07-02 09:32:41 +0300

    delete j part

--- core_job/core/userpart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import create_user, make_match, UsersListView
-
-
-urlpatterns = [
-    path("api/clients/create", create_user),
-    path("api/<int:match>/match", make_match),
-    path("api/list", UsersListView.as_view()),
-]
